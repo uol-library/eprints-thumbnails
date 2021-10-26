b15d410bd1119fde05ee24cd133097681c90c730
Peter Edwards <pete@bjorsq.net>
2021-10-26 16:43:40 +0100

    Changed audio thumbnailing

--- lib/plugins/EPrints/Plugin/Export/ThumbnailInfo.pm
+++ lib/plugins/EPrints/Plugin/Export/ThumbnailInfo.pm
@@ -76,13 +76,13 @@ sub output_dataobj
 		if ( $related->count > 0 )
 		{
 			$related->map( sub {
+				my( $session, $dataset, $eprintdoc, $rels ) = @_;
 				if ( $doc->get_value( 'format' ) eq 'audio' )
 				{
 					$rels->{'mp3'} = $eprintdoc->get_url();
 				}
 				else
 				{
-					my( $session, $dataset, $eprintdoc, $rels ) = @_;
 					(my $thumbname = $eprintdoc->value( 'main' )) =~ s/\.[^.]+$//;
 					my $thumbpos  = $doc->value( 'pos' );
 					my $thumbpl   = $repo->{config}->{http_url} . '/' . $eprint->value( 'eprintid' ) . '/' . $thumbpos . '.has' . $thumbname . 'ThumbnailVersion/' . $doc->get_value( 'main' );
