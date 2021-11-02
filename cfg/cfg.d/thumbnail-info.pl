c2e7f12cc4f1e512481820cef50cd6179e6e6e2f
Peter Edwards <pete@bjorsq.net>
2021-11-02 21:34:07 +0000

    added more logging

--- cfg/cfg.d/thumbnail-info.pl
+++ cfg/cfg.d/thumbnail-info.pl
@@ -3,18 +3,23 @@ $c->{on_generate_thumbnails} = sub
 {
     my ($session, $doc) = @_;
     my $eprint = $doc->parent;
-    my $dir  = $session->get_repository->config( "variables_path" ) . '/thumbs';
+    my $dir  = $session->get_repository->config( 'variables_path' ) . '/thumbs';
     my $id   = $eprint->value( 'eprintid' );
     # log function call
-    my $logfile = "$dir/on_generate_thumbnails.log";
-    if ( open(my $fha, '>>', $logfile) )
+    my $logfile = $dir . '/on_generate_thumbnails.log';
+    if ( open( my $fha, '>>', $logfile ) )
     {
         print $fha sprintf "%s on_generate_thumbnails called for eprint ID: %d\n", scalar localtime, $id;
-        close($fha);
+        close( $fha );
     }
     my $file = "$dir/$id.json";
     if ( -e $file )
     {
+        if ( open( my $fha, '>>', $logfile ) )
+        {
+            print $fha sprintf "JSON cache deleted for eprint ID: %d\n", $id;
+            close( $fha );
+        }
         unlink $file;
     }
 }
