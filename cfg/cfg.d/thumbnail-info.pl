e1167eb9970da6b74c56b965a024e0ee534f96fa
Peter Edwards <pete@bjorsq.net>
2021-11-02 17:12:05 +0000

    added logging to on_generate_thumbnails

--- cfg/cfg.d/thumbnail-info.pl
+++ cfg/cfg.d/thumbnail-info.pl
@@ -5,6 +5,13 @@ $c->{on_generate_thumbnails} = sub
     my $eprint = $doc->parent;
     my $dir  = $session->get_repository->config( "variables_path" ) . '/thumbs';
     my $id   = $eprint->value( 'eprintid' );
+    # log function call
+    my $logfile = "$dir/on_generate_thumbnails.log";
+    if ( open(my $fha, '>>', $logfile) )
+    {
+        print $fha sprintf "%s on_generate_thumbnails called for eprint ID: %d\n", scalar localtime, $id;
+        close($fha);
+    }
     my $file = "$dir/$id.json";
     if ( -e $file )
     {
