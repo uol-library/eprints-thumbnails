61d62081bad028d67a33f0fabaf122abd8d4f504
peteredwards <p.l.edwards@leeds.ac.uk>
2023-09-15 10:50:46 +0100

    Changed export to include more info

--- lib/plugins/EPrints/Plugin/Export/ThumbnailInfo.pm
+++ lib/plugins/EPrints/Plugin/Export/ThumbnailInfo.pm
@@ -1,7 +1,7 @@
 package EPrints::Plugin::Export::ThumbnailInfo;
 
 use EPrints::Plugin::Export::TextFile;
-use Data::Dumper;
+use Image::ExifTool;
 use JSON;
 
 @ISA = ( "EPrints::Plugin::Export::TextFile" );
@@ -28,6 +28,7 @@ sub output_dataobj
 	my( $plugin, $eprint ) = @_;
 
 	my $repo = $plugin->repository;
+	my $exiftool = new Image::ExifTool;
 
 	my $data = {
 		'eprintid' => $eprint->value( 'eprintid' ),
@@ -66,28 +67,48 @@ sub output_dataobj
 				if ( $doc->get_value( 'format' ) eq 'audio' )
 				{
 					$rels->{'url'} = $eprintdoc->get_url();
-					$rels->{'permalink'}   = $repo->{config}->{http_url} . '/' . $eprint->value( 'eprintid' ) . '/' . $thumbpos . '.hasaudio_mp3ThumbnailVersion/' . $doc->get_value( 'main' );
-
+					$rels->{'permalink'}   = $repo->{config}->{https_url} . '/' . $eprint->value( 'eprintid' ) . '/' . $thumbpos . '.hasaudio_mp3ThumbnailVersion/' . $doc->get_value( 'main' );
 				}
 				else
 				{
 					(my $thumbname = $eprintdoc->value( 'main' )) =~ s/\.[^.]+$//;
-					my $thumbpl   = $repo->{config}->{http_url} . '/' . $eprint->value( 'eprintid' ) . '/' . $thumbpos . '.has' . $thumbname . 'ThumbnailVersion/' . $doc->get_value( 'main' );
-					my $thumb = {
+					my $thumbpl   = $repo->{config}->{https_url} . '/' . $eprint->value( 'eprintid' ) . '/' . $thumbpos . '.has' . $thumbname . 'ThumbnailVersion/' . $doc->get_value( 'main' );
+					my $relfileobj = $eprintdoc->stored_file( $eprintdoc->get_main );
+					my $relfilepath = '' . $relfileobj->get_local_copy;
+					my $relinfo = $exiftool->ExtractInfo( $relfilepath, { 'FastScan' => 5 } );
+					my $relimginfo = $exiftool->GetInfo('ImageWidth', 'ImageHeight');
+					$rels->{$thumbname} = {
 						'url'       => $eprintdoc->get_url(),
 						'format'    => $eprintdoc->value( 'mime_type' ),
 						'name'      => $thumbname,
 						'permalink' => $thumbpl,
+						'width'     => 200,#$relimginfo->{'ImageWidth'},
+						'height'    => 200,#$relimginfo->{'ImageHeight'},
 					};
-					$rels->{$thumbname} = $thumbpl;
 				}
 			}, \%rels );
 		}
-		push @canvases, {
-			'url'        => $doc->get_url(),
-			'format'     => $doc->get_value( 'mime_type' ),
-			'thumbnails' => \%rels
-		};
+		my $fileobj  = $doc->stored_file( $doc->get_main );
+		my $filepath = '' . $fileobj->get_local_copy;
+		my $fileinfo = $exiftool->ExtractInfo( $filepath, { 'FastScan' => 5 } );
+		if ( $doc->get_value( 'format' ) eq 'audio' ) {
+			my $mp3info         = $exiftool->GetInfo( 'Duration#' );
+			push @canvases, {
+				'url'         => $doc->get_url(),
+				'format'      => $doc->get_value( 'mime_type' ),
+				'derivatives' => \%rels,
+				'duration'    => sprintf( '%d', $mp3info->{'Duration #'} )
+			};
+		} elsif ($doc->get_value( 'format' ) eq 'image' ) {
+			my $imginfo       = $exiftool->GetInfo('ImageWidth', 'ImageHeight');
+			push @canvases, {
+				'url'         => $doc->get_url(),
+				'format'      => $doc->get_value( 'mime_type' ),
+				'derivatives' => \%rels
+				'width'       => 200,#$imginfo->{'ImageWidth'},
+				'height'      => 200,#$imginfo->{'ImageHeight'},
+			};
+		}
 	}
 
 	$data->{items} = \@canvases;
